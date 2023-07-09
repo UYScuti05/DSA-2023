ecbe153442e82439b48d8b6144dfd7255769413a
UY Scuti <anshul.sharma05@outlook.com>
2023-07-09 18:40:20 +0530

    Success

--- start.cpp
+++ start.cpp
@@ -1,72 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
-struct Node {
-    int data;
-    struct Node* next;
-    Node(int data)
-    {
-        this->data = data;
-        next = NULL;
-    }
-};
- 
-struct LinkedList {
-    Node* head;
-    LinkedList() { head = NULL; }
- 
-    void reverse()
-    {
-        // My soultion
-        // Initializing variables
-        Node* current = head;
-        Node* previous = NULL;
 
-        Node* temp;
-        while(current!=NULL){
-            temp = current;
-            current = temp->next;
-            temp->next = previous;
-            previous = temp;
-        }
-        head = previous;
+vector<int> solution(vector<int> &v){
+    set<int> s;
+    for(auto it:v){
+        s.insert(it);
     }
- 
-    void print()
-    {
-        struct Node* temp = head;
-        while (temp != NULL) {
-            cout << temp->data << " ";
-            temp = temp->next;
+    vector<int> result;
+    for(int i=1;i<=v.size();i++){
+        if(s.find(i)!=s.end()){
+
+        }else{
+            result.push_back(i);
         }
     }
- 
-    void push(int data)
-    {
-        Node* temp = new Node(data);
-        temp->next = head;
-        head = temp;
-    }
-};
- 
+    return result;
+}
+
 int main()
 {
-    LinkedList ll;
-    ll.push(20);
-    ll.push(4);
-    ll.push(15);
-    ll.push(85);
-
-    ll.push(100);
-    ll.push(102);
-    ll.push(105);
-    ll.push(108);
-    cout << "Given linked list\n";
-    ll.print();
- 
-    ll.reverse();
- 
-    cout << "\nReversed linked list \n";
-    ll.print();
+    vector<int> v = {4,3,2,7,8,2,3,1};
+    vector<int> result = solution(v);
+    for(auto it: result){
+        cout<<it<<endl;
+    }
     return 0;
 }
