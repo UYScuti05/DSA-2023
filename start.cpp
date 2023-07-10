7499170d6dc4a5696289b1d1da75b9069823709e
UY Scuti <anshul.sharma05@outlook.com>
2023-07-10 20:48:44 +0530

    Success

--- start.cpp
+++ start.cpp
@@ -1,27 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> solution(vector<int> &v){
-    set<int> s;
-    for(auto it:v){
-        s.insert(it);
-    }
-    vector<int> result;
-    for(int i=1;i<=v.size();i++){
-        if(s.find(i)!=s.end()){
-
-        }else{
-            result.push_back(i);
+void solution(vector<char> &s){
+    for(int i=0;i<s.size()/2;i++){
+            char temp = s[i];
+            s[i] = s[s.size()-1-i];
+            s[s.size()-1-i] = temp;
         }
-    }
-    return result;
 }
 
 int main()
 {
-    vector<int> v = {4,3,2,7,8,2,3,1};
-    vector<int> result = solution(v);
-    for(auto it: result){
+    vector<char> v = {'H','E','L','L','O','O'};
+    solution(v);
+    for(auto it: v){
         cout<<it<<endl;
     }
     return 0;
--- tempCodeRunnerFile.cpp
+++
@@ -1,3 +0,0 @@
-ll.push(4);
-    // ll.push(15);
-    // ll.push(85);
