e7227bfcc88dfa1c33e00294e9abf6e6f8763a5f
UY Scuti <anshul.sharma05@outlook.com>
2023-07-15 18:58:11 +0530

    Success

--- start.cpp
+++ start.cpp
@@ -1,51 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<int>> permute(vector<int> &nums){
-    if(nums.size()==1){
-        vector<int> temp = {nums[0]};
-        vector<vector<int>> result = {temp};
-        return result;
-    }
-
-    vector<vector<int>> result;
-
-
-    for(int i=0;i<nums.size();i++){
-        vector<int> restArray;
-        for(int j=0;j<nums.size();j++){
-            if(j==i){
-
+bool isValid(string s){
+    stack<char> m;
+    for(int i=0;i<s.length();i++){
+        if(s[i]=='('||s[i]=='{'||s[i]=='['){
+            m.push(s[i]);
+        }else{
+            char temp;
+            if(s[i]==')'){
+                temp = '(';
+            }
+            if(s[i]=='}'){
+                temp = '{';
+            }
+            if(s[i]==']'){
+                temp = '[';
+            }
+            if(m.size()==0) return false;
+            if(m.top()==temp){
+                m.pop();
             }else{
-                restArray.push_back(nums[j]);
+                return false;
             }
-        }
-
-        vector<vector<int>> temp = permute(restArray);
-        for(int j=0;j<temp.size();j++){
-            vector<int> isntance = temp[j];
-            vector<int> resultInst;
-            resultInst.push_back(nums[i]);
-            for(int k=0;k<isntance.size();k++){
-                resultInst.push_back(isntance[k]);
-            }
-            result.push_back(resultInst);
         }
     }
-    return result;
-
+    if(m.size()==0)return true;
+    return false;
 }
-
 int main()
 {
-	vector<int> nums = {1,2,3,4,5,6,7,8,9,10};
-    vector<vector<int>> result = permute(nums);
-    for(int i=0;i<result.size();i++){
-        vector<int> temp = result[i];
-        for(int j=0;j<temp.size();j++){
-            cout<<temp[j]<<" ";
-        }
-        cout<<endl;
-    }
+	string str = "(]";
+    cout<<isValid(str);
 	return 0;
 }
