db7fe43004ceda17341d9264787a7e0de2c8228d
UY Scuti <anshul.sharma05@outlook.com>
2023-07-15 18:41:02 +0530

    Success

--- start.cpp
+++ start.cpp
@@ -1,20 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
-map<int,int> dp;
-int climbStairs(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        if(n==2) return 2;
-        if(dp.find(n)!=dp.end()){
-            return dp[n];
-        }else{
-            dp[n] = climbStairs(n-1)+climbStairs(n-2);
-        }
-        return dp[n];
+
+vector<vector<int>> permute(vector<int> &nums){
+    if(nums.size()==1){
+        vector<int> temp = {nums[0]};
+        vector<vector<int>> result = {temp};
+        return result;
     }
+
+    vector<vector<int>> result;
+
+
+    for(int i=0;i<nums.size();i++){
+        vector<int> restArray;
+        for(int j=0;j<nums.size();j++){
+            if(j==i){
+
+            }else{
+                restArray.push_back(nums[j]);
+            }
+        }
+
+        vector<vector<int>> temp = permute(restArray);
+        for(int j=0;j<temp.size();j++){
+            vector<int> isntance = temp[j];
+            vector<int> resultInst;
+            resultInst.push_back(nums[i]);
+            for(int k=0;k<isntance.size();k++){
+                resultInst.push_back(isntance[k]);
+            }
+            result.push_back(resultInst);
+        }
+    }
+    return result;
+
+}
+
 int main()
 {
-	int n = 5;
-    cout<<climbStairs(n)<<endl;
+	vector<int> nums = {1,2,3,4,5,6,7,8,9,10};
+    vector<vector<int>> result = permute(nums);
+    for(int i=0;i<result.size();i++){
+        vector<int> temp = result[i];
+        for(int j=0;j<temp.size();j++){
+            cout<<temp[j]<<" ";
+        }
+        cout<<endl;
+    }
 	return 0;
 }
